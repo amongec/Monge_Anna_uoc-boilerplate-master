68bea21ab9c178779c0f5525cbe0d64fbbd4da88
amongec <44950780+amongec@users.noreply.github.com>
2023-10-29 11:52:27 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
 module.exports  =  { 
   syntax : 'postcss-lit' , 
   plugins : { 
+    'postcss-import': {},
+    'tailwindcss': {},
+    'autoprefixer': {},
+    'tailwindcss/nesting': {},
+    'tailwindcss': {},
+    'autoprefixer': {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
     require('tailwindcss'),
+    require('autoprefixer'),
   } 
 } ;
